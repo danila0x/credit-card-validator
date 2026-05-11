4d52e886fbda79c0b932e265f38221b93dfaf0de
Danila Potapeshkin <danilapotapeshkin2@gmail.com>
2026-05-11 20:20:16 +0300

    firts commit

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Bank struct {
+	Name   string
+	Prefix string
+}
+
+func main() {
+	banks := []Bank{
+		{Name: "Lunar Bank", Prefix: "4000"},
+		{Name: "Mars Credit Union", Prefix: "5000"},
+		{Name: "Venus Express", Prefix: "6000"},
+		{Name: "Saturn Ring", Prefix: "7000"},
+		{Name: "Jupiter Trust", Prefix: "8000"},
+	}
+	cardNumber := "4000123456789017"
+	fmt.Println(DetectBank(cardNumber, banks))
+	fmt.Println(LuhnCheck(cardNumber))
+
+}
+
+func LuhnCheck(cardNumber string) bool {
+	if cardNumber == "" {
+		return false
+	}
+	sum := 0
+	isSecond := false
+	for i := len(cardNumber) - 1; i >= 0; i-- {
+		digit := int(cardNumber[i] - '0')
+		if digit < 0 || digit > 9 {
+			return false
+		}
+		if isSecond {
+			digit *= 2
+			if digit > 9 {
+				digit -= 9
+			}
+		}
+		sum += digit
+		isSecond = !isSecond
+	}
+	return sum%10 == 0
+}
+
+func DetectBank(cardNumber string, banks []Bank) *Bank {
+	if cardNumber == "" {
+		return nil
+	}
+	for i := range banks {
+		if strings.HasPrefix(cardNumber, banks[i].Prefix) {
+			return &banks[i]
+		}
+	}
+	return nil
+}
